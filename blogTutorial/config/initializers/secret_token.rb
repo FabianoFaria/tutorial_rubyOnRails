a7bd5d74918c56ac913fc864081b628d86af1886
FabianoFaria <fabiano@joiss.com.br>
2015-08-11 19:49:13 -0300

    primeiro commit rubyOnRails

---
+++ blogTutorial/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogTutorial::Application.config.secret_token = 'a29f8d05095242468f68dd90681cd7a773687d59eece99e2fbda0fc7fe0a76da37d4964af406b1f966288bb13a53b27c316be37486faf4731005a1699b520c4f'
